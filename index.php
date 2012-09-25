126bd94e4d07dfd171602f92bafe350fd8b835f0
Ben Del Vacchio <delvach@gmail.com>
2012-09-25 17:19:19 -0400

    Adding header and footer .php files

---
+++ index.php
@@ -0,0 +1,19 @@
+<? 
+
+$PAGE_ID = 'home';
+
+include("lib/header.inc"); ?>
+
+<div id="content">
+
+
+    <div id="alpha">
+    
+        <p>Welcome to the skeletal version of my future website, currently being assembled using semantic HTML 5!</p>
+        
+    </div>
+
+
+</div>
+
+<? include('lib/footer.inc'); ?>
